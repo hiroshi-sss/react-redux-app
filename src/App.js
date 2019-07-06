cab90a32f978f397ca47eff661696ca545301fd2
hiroshi <hiroshi.weblog@gmail.com>
2019-07-06 19:26:55 +0900

    leane jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <header>
-        <p>
-          Hello, World
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+
+      const greeting = "Hi, Tom!";
+      const dom = <h1 className="foo">{greeting}</h1>;
+
+      return (
+        <React.Fragment>
+          <label htmlFor="bar">{dom}</label>
+          <input type ="text" onChange={() => {console.log("Clicked")}} />
+        </React.Fragment>
+    )
+
+  }
 }
-
 export default App;
