5fe0d475ab761761b5bc03415178b379cb4c4dc1
hiroshi <hiroshi.weblog@gmail.com>
2019-10-19 20:21:08 +0900

    src delete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import React, { Component } from 'react';
-
-// class App extends Component {
-//   render() {
-//
-//       const greeting = "Hi, Tom!";
-//       const dom = <h1 className="foo">{greeting}</h1>;
-//
-//       return (
-//         <React.Fragment>
-//           <label htmlFor="bar">{dom}</label>
-//           <input type ="text" onChange={() => {console.log("Clicked")}} />
-//         </React.Fragment>
-//     )
-//
-//   }
-// }
+import React from 'react';
 
 const App = () => {
   const profiles = [
     { name: "Taro", age: 10 },
     { name: "hanako", age: 5},
-    {  }
+    { name: 'aaa' }
   ]
 
   return (
@@ -37,7 +21,7 @@ const User = (props) => {
   return <div>Hi, I am {props.name}, and {props.age} years old!</div>
 }
 User.defaultProps = {
-  name: "???" , age: "???"
+  name: '???' , age: "???"
 }
 
 export default App;
