a51f3c92fa4c2a38c166b56966be0273a2a0a3fe
hiroshi <hiroshi.weblog@gmail.com>
2019-11-02 10:08:01 +0900

    implement eventsShow

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 export const READ_EVENTS = "READ_EVENTS";
+export const READ_EVENT = "READ_EVENT";
 export const CREATE_EVENTS = "CREATE_EVENTS";
 export const DELETE_EVENTS = "DELETE_EVENTS";
+export const UPDATE_EVENTS = "UPDATE_EVENTS";
 
 const ROOT_URL = "https://udemy-utils.herokuapp.com/api/v1";
 const QUERYSTRING = "?token=token123";
@@ -16,6 +18,15 @@ export const postEvents = values => async dispatch => {
   const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values);
   dispatch({ type: CREATE_EVENTS, response });
 };
+
+export const putEvent = values => async dispatch => {
+    const response = await axios.put(`${ROOT_URL}/events/${values.id}${QUERYSTRING}`, values)
+    dispatch({ type: UPDATE_EVENTS, response })
+}
+export const getEvent = id => async dispatch => {
+    const response = await axios.get(`${ROOT_URL}/events/${id}${QUERYSTRING}`)
+    dispatch({ type: READ_EVENT, response })
+}
 
 export const deleteEvents = id => async dispatch => {
   const response = await axios.delete(`${ROOT_URL}/events/${id}${QUERYSTRING}`);
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,15 +1,19 @@
-import { READ_EVENTS, DELETE_EVENTS } from "../actions";
+import { READ_EVENTS, DELETE_EVENTS, READ_EVENT, UPDATE_EVENTS, CREATE_EVENTS } from "../actions";
 import _ from "lodash";
 
 export default (events = {}, action) => {
   switch (action.type) {
+    case CREATE_EVENTS:
+    case READ_EVENT:
+    case UPDATE_EVENTS:
+      const data = action.response.data;
+      return { ...events, [data.id]: data };
     case READ_EVENTS:
-      return _.mapKeys(action.response.data, "id")
+      return _.mapKeys(action.response.data, "id");
     case DELETE_EVENTS:
-      delete events[action.id]
-      return { ...events }
+      delete events[action.id];
+      return { ...events };
     default:
       return events;
-    
   }
 };
